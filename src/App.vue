0a018dcc73da8dedfb18c9f9ad8561ca4837c1fe
Robert Andrade <srandradejr@gmail.com>
2021-11-27 17:03:59 +0000

    added resume info

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,25 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 <style>
 :root {
-	--navy: #0c0c4a;
-	--dark-navy: #060726;
-	--pink: rgba(213, 84, 213, 1);
-	--pink2: rgba(213, 84, 213, 0.7);
+  --navy: #0c0c4a;
+  --dark-navy: #060726;
+  --pink: rgba(213, 84, 213, 1);
+  --pink2: rgba(213, 84, 213, 0.7);
 }
 #app {
-	width: 100%;
-	height: 75%;
-	display: grid;
-	place-items: center;
-	grid-template-columns: repeat(2, 1fr);
+  width: 100%;
+  height: 75%;
+  display: grid;
+  place-items: center;
+  grid-template-columns: repeat(2, 1fr);
 }
 
 body {
-	width: 100vw;
-	height: 100vh;
-	background: black;
-	display: grid;
-	place-items: center;
-	filter: saturate(130%) contrast(125%);
+  width: 100vw;
+  height: 100vh;
+  background: black;
+  display: grid;
+  place-items: center;
+  filter: saturate(130%) contrast(125%);
 }
 </style>
---
+++ src/components/ResumeInfo.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="flex">
+    <div class="left-col">
+      <h1>S. Robert Andrade Jr.</h1>
+      <h2>(763) 501-9532</h2>
+      <h2>robert.andrade.developer@gmail.com</h2>
+      <div class="experience">
+        <article class="role">
+          <h3>EXPERIENCE</h3>
+          <h4>Creative Technologist</h4>
+          <h5>Wiley, Minneapolis MN</h5>
+          <h6>November 2019 - Present</h6>
+          <p>
+            Provided technical leadership, mentorship, and development for business
+            essential applications leveraging social learning. Collaborated with User
+            Experience Designers to create fast, accessible, beautiful and meaningful
+            digital experiences
+          </p>
+        </article>
+        <article class="role">
+          <h4>Devops Product Engineer</h4>
+          <h5>Constellation, Edina MN</h5>
+          <h6>August 2019 - October 2019</h6>
+          <p>Used AWS, React, Typescript, Jenkings, Docker, CodeBuild, and Terraform to ensure application reliability and used development skills to build production ready components and pages</p>
+        </article>
+        <article class="role">
+          <h4>Software Engineer</h4>
+          <h5>Riley Hayes Advertising, Minneapolis, MN</h5>
+          <h6>November 2018 - August 2019</h6>
+          <p>
+            Full Stack development for both customer facing and employee facing
+            applications. Lead developer for multiple Best Buy campaigns and
+            websites for clients such as Delta Airlines and Nuvectra.
+          </p>
+        </article>
+        <article class="role">
+          <h4>Full-Stack Engineering Student</h4>
+          <h5>Prime Digital Academy, Minneapolis MN</h5>
+          <h6>April 2018 - August 2018</h6>
+          <p>
+            Intensive 20 week full time bootcamp teaching applicable technologies for
+            modern software engineering. Also teaches how to scope projects, how to
+            execute on longform projects, business analytics, along with soft skills
+            such as public speaking and how to be successful in the workplace.
+          </p>
+        </article>
+      </div>
+      <div class="education">
+        <h3>EDUCATION</h3>
+        <div class="school">
+          <h4>Bachelor of Arts, French Studies & American History</h4>
+          <h5>University of Minnesota - Twin Cities Minneapolis MN</h5>
+          <h6>September 2010 - May 2014</h6>
+        </div>
+      </div>
+      <div class="volunteer">
+        <h3>VOLUNTEER</h3>
+        <p>University of Minnesota - French Language Tutor (2012-2014)</p>
+        <p>
+          University of Minnesota - TANDEM Program teaching Korean students
+          English and facilitating transition to University life (2014)
+        </p>
+      </div>
+    </div>
+    <div class="right-col">
+      <div class="technologies">
+        <h3>TECHNOLOGIES</h3>
+        <p>
+          Git, GitHub
+          JavaScript, HTML, CSS,
+          Gulp, Sass, Handlebars.js
+          PHP, Laravel, Artisan
+          React, Vue.js
+          MongoDB, Mongoose
+          PostgreSQL, MySQL
+          Node.js, Express, Ajax
+          Python, Flask
+        </p>
+        <h3>LANGUAGES</h3>
+      </div>
+      <div class="languages">
+        <p>
+          English(USA) - Fluent, Native
+          Proficiency
+          Spanish (Mexico) - Fluent,
+          Native proficiency
+          French (France) -
+          Professional Working
+          Proficiency
+          Korean - Conversational
+        </p>
+      </div>
+      <div class="links">
+        <h3>LINKS</h3>
+        <p>
+          LinkedIn:
+          www.linkedin.com/in/randradejr/
+          GitHub:
+          github.com/RobertAndradeJr
+          Personal Site:
+          robertandradejr.dev
+        </p>
+      </div>
+    </div>
+    <div class="bottom">
+      <h3>PROJECTS</h3>
+      <div class="projects">
+        <h4>Everything DiSC on Catalyst</h4>
+        <h5>Vue.js | Sass | Jest | Jenkins | Azure SQL Database</h5>
+        <p>
+          Lead front end developer on a project that revitalized revenue streams during the pandemic. New
+          simpler to use, beautiful, and responsive webapp helped attract a new client client base and increased return
+          customers all while augmenting revenue streams while competitors saw market share shrink. Utilized Agile
+          methodology to deliver 100% on committed business objectives while implementing WCAG 2.0 A conformance
+          Best Buy CitiBank Email
+        </p>
+        <h4>Best Buy CitiBank Email</h4>
+        <h5>HTML | CSS | Sass | Gulp | Mjml | Handlebars</h5>
+        <p>
+          Developed a custom framework for building marketing emails that are identical and effective across all
+          email platforms. Integrated multiple disparate tools to double our efficiency and accuracy. Emails have more
+          than 1,000,000 views each campaign and clickthrough rate has doubled since our framework has enabled us to
+          use more attractive designs. Also developed landing pages served to over 1,000,000 users describing benefits of
+          the Best Buy Visa Card.
+        </p>
+        <h4>DeltaAmExCard.com</h4>
+        <h5>Laravel | Artisan | Javascript | Jquery | Sass | AWS</h5>
+        <p>
+          Developed a website serving unique credit card offers for prospective American Express Skymiles credit
+          card holders. Serving over 2,000,000 unique users. Also helped create AmEx lookup tool to aid CS
+          representatives give clear and pertinent information to prospective customers. Over 1,000,000 new
+          cardmembers.
+        </p>
+        <h4>311-AI - CodeSwitch.mn 2018 2nd Place Winner</h4>
+        <h5>
+          CodeSwitch.mn 2018 2CodeSwitch.mn 2018 2
+          nd Place Winner
+        </h5>
+        <p>
+          Homelessness is a huge problem in the Twin-Cities area. 311-AI aims to help solve that problem by
+          offloading some of the issues people experiencing homelessness have to an AI chatbot. This way people
+          experiencing homeless can ask simple questions to a chatbot that has up-to-date information and social
+          workers can focus more time on helping people with more complicated requests. I was in charge of training
+          Google Dialog Flow intents and the front end along with managing the structure of the project in React.
+        </p>
+        <h4>Prime Scholarship Fund</h4>
+        <h5>React | Express | Node.js | PostgreSQL</h5>
+        <p>
+          Currently there is no way for Prime Digital Academy students to cover their living costs throughout the
+          14 weeks that they attend the in-class portion Prime Digital Academy. Working in a team of 4 developers, we
+          built the online presence for this Scholarship Fund and created a tool for prospective students to apply and for
+          the board of the Scholarship Fund to manage and fundraise quickly and efficiently. I was completely in charge
+          of front end design and logic. From picking themes to connecting to APIs I did everything that was not server or
+          database related.
+        </p>
+      </div>
+      <div class="community">
+        <h3>COMMUNITY ENGAGEMENT</h3>
+        <h4>Minneapolis Jr. Devs</h4>
+        <p>Attendee</p>
+        <h4>AngularMN</h4>
+        <p>attendee</p>
+        <h4>React Minneapolis Meetup</h4>
+        <p>Attendee</p>
+        <h4>CodeSwitchMN</h4>
+        <p>Participant - 2nd place award recipient</p>
+      </div>
+    </div>
+  </div>
+</template>
